cce097590a20b3aa11df62aa4ba787eb1ac0e3ea
rajdangi31 <dangiprince263@gmail.com>
2026-03-03 14:49:33 -0500

    Set-up

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        systemBlack: '#000000',
+        systemNeon: '#00E5FF', // That iconic Solo Leveling blue
+        systemGray: '#1A1A1A',
+      },
+    },
+  },
+  plugins: [],
+}
